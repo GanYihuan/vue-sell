d7c0b4359c65d3cad965c55f984cfe2c4526fbae
GanYihuan <583520052@qq.com>
2018-08-23 07:30:00 +0800

    7-15, 7-16, 7-17: cartcontrol 组件

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -26,7 +26,7 @@ export default {
 	methods: {
 		addCart(event) {
 			/* better-scroll */
-      /* 在PC将触发两次事件，停止这个 */
+			/* 在PC将触发两次事件，停止这个 */
 			if (!event._constructed) {
 				return
 			}
@@ -39,8 +39,8 @@ export default {
 			this.$emit('add', event.target)
 		},
 		decreaseCart(event) {
-      /* better-scroll */
-      /* 在PC将触发两次事件，停止这个 */
+			/* better-scroll */
+			/* 在PC将触发两次事件，停止这个 */
 			if (!event._constructed) {
 				return
 			}
